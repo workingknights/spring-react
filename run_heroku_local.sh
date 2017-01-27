674f5b6c80d3492e014eb8ac8fe0f535602c7d89
workingknights <anthony.knights@gmail.com>
2017-01-27 16:45:29 +0900

    updated properties

--- run_heroku_local.sh
+++ run_heroku_local.sh
@@ -4,4 +4,10 @@ if [ $1 = b ]; then
     mvn clean install
 fi
 
+#export DATABASE_URL=jdbc:postgresql://uidxjooylvwuox:3d0731cf9e6444c5be77ac0f1e019ee7a021b01311f4e411e5a1e46a7c299cc3@ec2-54-225-127-147.compute-1.amazonaws.com:5432/d22l5h0da3nibd
+#export SPRING_DATASOURCE_URL=jdbc:postgresql://ec2-54-225-127-147.compute-1.amazonaws.com:5432/d22l5h0da3nibd?sslmode=require&user=uidxjooylvwuox&password=3d0731cf9e6444c5be77ac0f1e019ee7a021b01311f4e411e5a1e46a7c299cc3
+export JDBC_DATABASE_URL="jdbc:postgresql://ec2-54-225-127-147.compute-1.amazonaws.com:5432/d22l5h0da3nibd?sslmode=require"
+export JDBC_DATABASE_USERNAME="uidxjooylvwuox"
+export JDBC_DATABASE_PASSWORD="3d0731cf9e6444c5be77ac0f1e019ee7a021b01311f4e411e5a1e46a7c299cc3"
+
 heroku local web
--- spring-react-be/src/main/java/name/aknights/payroll/SpringReactApplication.java
+++ spring-react-be/src/main/java/name/aknights/payroll/SpringReactApplication.java
@@ -1,23 +1,29 @@
 package name.aknights.payroll;
 
-import name.aknights.App;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.sql.DataSource;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class SpringReactApplication implements ApplicationListener<ContextRefreshedEvent> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SpringReactApplication.class);
 
     @Value("${spring.profiles.active}")
     protected String springProfilesActive;
@@ -44,4 +50,22 @@ public class SpringReactApplication implements ApplicationListener<ContextRefres
             }
         };
     }
+//
+//    @Configuration
+//    public class MainConfig {
+//
+//        @Bean
+//        public BasicDataSource dataSource() throws URISyntaxException {
+//            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//            String username = System.getenv("JDBC_DATABASE_USERNAME");
+//            String password = System.getenv("JDBC_DATABASE_PASSWORD");
+//
+//            BasicDataSource basicDataSource = new BasicDataSource();
+//            basicDataSource.setUrl(dbUrl);
+//            basicDataSource.setUsername(username);
+//            basicDataSource.setPassword(password);
+//
+//            return basicDataSource;
+//        }
+//    }
 }
